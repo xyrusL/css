88f9910c9da22adcf661512054ab04cad1012941
Xyrus <91442589+xyrusL@users.noreply.github.com>
2024-12-24 21:59:43 +0800

    Update and rename fetch.js to detch.js

--- src/detch.js
+++ src/detch.js
@@ -8,7 +8,7 @@ function fetchAllData() {
   axios.get(url, { headers: headers })
     .then(response => {
       const animeData = response.data;
-      localStorage.setItem(storageKey, encryptData(animeData));
+      localStorage.setItem('animeData', encryptData(animeData));
       console.log('Data fetched and stored securely.');
     })
     .catch(error => {
